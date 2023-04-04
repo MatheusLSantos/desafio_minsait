829d7d79c53cedea8853f68fb91dd5d4b551b1b7
Matheus L Santos <matheus2737p@gmail.com>
2023-04-04 18:41:29 +0000

    FIX create_env_all.sh

--- input/desafio_curso/scripts/HDFS/create-hdfs-folders.sh
+++ input/desafio_curso/scripts/HDFS/create-hdfs-folders.sh
@@ -5,10 +5,3 @@ for pasta in "${pastas[@]}"
 do
 	hdfs dfs -mkdir /datalake/"$pasta"
 done
-arquivos=($(cd /input/desafio_curso/raw && ls))
-sufix=".csv"
-for arquivo in "${arquivos[@]}"
-do
-	hdfs dfs -mkdir /datalake/raw/${arquivo%$sufix}
-	hdfs dfs -copyFromLocal /input/desafio_curso/raw/$arquivo /datalake/raw/${arquivo%$sufix}
-done
--- input/desafio_curso/scripts/pre_process/create_env_all.sh
+++ input/desafio_curso/scripts/pre_process/create_env_all.sh
@@ -10,6 +10,6 @@ do
     cd ../../raw/
     hdfs dfs -mkdir /datalake/raw/$i
     hdfs dfs -chmod 777 /datalake/raw/$i
-    hdfs dfs -copyFromLocal -n $i.csv /datalake/raw/$i
-    beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
+    hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
+    beeline -u jdbc:hive2://localhost:10000 -f ../scripts/hql/create_table_$i.hql 
 done
